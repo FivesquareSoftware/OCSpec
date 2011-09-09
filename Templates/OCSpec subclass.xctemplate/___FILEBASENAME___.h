d5d9a1182c3187f654e97f5608c3e7b78e702cb9
John Clayton <john@fivesquare.net>
2011-09-08 17:26:36 -0700

    Removed some unused resources from spec runner and moved source, upgraded templates for xcode 4

---
+++ Templates/OCSpec subclass.xctemplate/___FILEBASENAME___.h
@@ -0,0 +1,19 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "OCExampleGroup.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : OCExampleGroup {
+ // add any ivars you need to store state for specs
+}
+
+// add properties as needed
+
+@end
---
+++ Templates/OCSpec subclass.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,59 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+___OPTIONALHEADERIMPORTLINE___
+
+// This class will be instantiated for you and made available in the property "self.specHelper", store your cross-test data and helper methods there
+#import "SpecHelper.h"
+
+
+#import <SenTestingKit/SenTestingKit.h>
+#import "OCMock.h"
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
++ (NSString *)description {
+    return @"Some description of this example group";
+}
+
+// ========================================================================== //
+
+#pragma mark -
+#pragma mark Setup and Teardown
+
+
+- (void)beforeAll {
+    // set up resources common to all examples here
+}
+
+- (void)beforeEach {
+    // set up resources that need to be initialized before each example here 
+}
+
+- (void)afterEach {
+    // tear down resources specific to each example here
+}
+
+
+- (void)afterAll {
+    // tear down common resources here
+}
+
+// ========================================================================== //
+
+#pragma mark -
+#pragma mark Specs
+
+- (void)shouldFail {
+	STAssertTrue(NO,@"Unimplemented");
+}
+
+
+
+
+@end
